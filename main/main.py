c6a5b9b0d5607cc001cc17d13f143cff65c4b334
wy2266336 <36040477+wy2266336@users.noreply.github.com>
2024-09-30 22:40:47 -0400

    Update main.py

--- main/main.py
+++ main/main.py
@@ -5,7 +5,7 @@ import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 def parse():
-    parser = argparse.ArgumentParser(description="tree transformer")
+    parser = argparse.ArgumentParser(description="longformer")
     parser.add_argument('-task_name',default='ner',help='the dataset name of task')
     parser.add_argument('-model_dir',default='train_model1',help='output model weight dir')
     parser.add_argument('-seq_length', type=int, default=100, help='sequence length')
@@ -18,11 +18,11 @@ def parse():
     parser.add_argument('-test', action='store_true',help='whether test')
     parser.add_argument('-test_sen', action='store_true', help='whether test a sentence')
     parser.add_argument('-text', default='', help='input a sentence')
-    parser.add_argument('-valid_path',default='data/BC2GM-IOB/devel.tsv',help='validation data path')
-    parser.add_argument('-train_path',default='data/BC2GM-IOB/train.tsv',help='training data path')
-    parser.add_argument('-test_path',default='data/BC2GM-IOB/test.tsv',help='testing data path')
-    #parser.add_argument('-adj_file', default='data/MLEE/ADJ.json',help='event label co-occurrence matrix path')
-    #parser.add_argument('-embed_file', default='data/MLEE/mlee_label_vec_300.json', help='event label embedding path')
+    parser.add_argument('-valid_path',default='data/',help='validation data path')
+    parser.add_argument('-train_path',default='data/',help='training data path')
+    parser.add_argument('-test_path',default='data/',help='testing data path')
+    #parser.add_argument('-adj_file', default='data/ADJ.json',help='event label co-occurrence matrix path')
+    #parser.add_argument('-embed_file', default='data/_label_vec_300.json', help='event label embedding path')
     parser.add_argument('-early_stop', default=100, help='early stop')
     parser.add_argument('-seed', type=int, default=12, help='random seed')
     args = parser.parse_args()
